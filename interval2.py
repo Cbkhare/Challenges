dd2e0ea0d602443ca98bb5b280fa8759b7996bba
Cbkhare <chitrabasukhare89@gmail.com>
2015-12-01 15:39:26 +0530

    HackerEarth

--- interval2.py
+++ interval2.py
@@ -7,6 +7,42 @@ class Solution:
         print (lst)
         i = 0
 
+        while len(lst)>1:
+            #for continous lst
+            print (lst,stack)
+            if lst[1][0] in range(lst[0][0] ,lst[0][1]+1):
+                # case 1 :- [1,3][1,6] = [1,6]
+                if lst[0][1]<=lst[1][1]:
+                    lst[0][1]= lst[1][1]
+                    if [lst[0][0],lst[1][1]] not in stack:
+                        stack.append([lst[0][0],lst[1][1]])
+                        #modify lst
+                    lst.pop(1)
+                # case 2 :- [1,6][1,3] = [1,6]
+                elif lst[0][1]>lst[1][1]:
+                    if lst[0] not in stack:
+                        stack.append(lst[0])
+                        #modify lst
+                    lst.pop(0)
+            #for breaking lst
+            #[1,3][4,5] = [1,3] in stack and continuin [4,5] in lst 
+            elif lst[1][0] not in range(lst[0][0] ,lst[0][1]+1):
+                if lst [0] in stack:
+                    lst.pop(0)
+                else:
+                    stack.append(lst.pop(0))
+                    
+                        
+        if lst[0] not in stack: stack.append(lst.pop(0))
+        '''
+        if stack[1][0] == stack[0][0]:
+            if stack[0][1]<=stack[1][1]:
+                stack[0][1]=stack[1][1]
+            stack.pop(1)
+        '''
+        return stack
+  
+        '''
         while i< len(lst)-1:
             if lst[i+1][0] in range(lst[i][i],lst[i][1]+1):
                 if lst[i][1]<=lst[i+1][1]:
@@ -23,7 +59,8 @@ class Solution:
             print(stack,lst)
         if len(stack)>=2:
             if stack[-2]<stack[-1]: stack.pop(0)
-        return stack         
+        return stack
+        '''
         '''
         while len(lst)>0:
             if len(lst)>=2:
