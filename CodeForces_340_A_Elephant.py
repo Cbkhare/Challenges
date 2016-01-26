3d33b7ef23ce47ec9baf664809a705a97874d8c1
Cbkhare <chitrabasukhare89@gmail.com>
2016-01-26 23:21:47 +0530

    cf

---
+++ CodeForces_340_A_Elephant.py
@@ -0,0 +1,59 @@
+
+if __name__ == '__main__':
+    c = int(input())
+    x = [5,4,3,2,1]
+    count = 0 
+    while x!=[]:
+        m = max(x)
+        while c>=max(x):
+            c=c-m
+            count+=1
+        x.pop(0)
+    print (count)
+    
+                
+            
+'''
+
+A. Elephant
+time limit per test
+1 second
+memory limit per test
+256 megabytes
+input
+standard input
+output
+standard output
+
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+Input
+
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+Output
+
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+Sample test(s)
+Input
+
+5
+
+Output
+
+1
+
+Input
+
+12
+
+Output
+
+3
+
+Note
+
+In the first sample the elephant needs to make one step of length 5 to reach the point x.
+
+In the second sample the elephant can get to point x if he moves by 3, 5 and 4. There are other ways to get the optimal answer but the elephant cannot reach x in less than three moves.
+
+'''
+    
