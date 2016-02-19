163c2d21677476dd1381d47d51caf2119882c3b7
Cbkhare <chitrabasukhare89@gmail.com>
2016-02-19 23:35:40 +0530

    CF

---
+++ CodeForces_630_C_LuckyNumbers.py
@@ -0,0 +1,38 @@
+
+if __name__=='__main__':
+    n = int(input())
+
+    print (pow(2,(n+1))-2)
+
+'''
+
+C. Lucky Numbers
+time limit per test
+0.5 seconds
+memory limit per test
+64 megabytes
+input
+standard input
+output
+standard output
+
+The numbers of all offices in the new building of the Tax Office of IT City will have lucky numbers.
+
+Lucky number is a number that consists of digits 7 and 8 only. Find the maximum number of offices in the new building of the Tax Office given that a door-plate can hold a number not longer than n digits.
+Input
+
+The only line of input contains one integer n (1 ≤ n ≤ 55) — the maximum length of a number that a door-plate can hold.
+Output
+
+Output one integer — the maximum number of offices, than can have unique lucky numbers not longer than n digits.
+Examples
+Input
+
+2
+
+Output
+
+6
+
+
+'''
