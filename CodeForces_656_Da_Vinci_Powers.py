77653b1fb1af353faae7a201d270ef7bd6c2ec19
Cbkhare <chitrabasukhare89@gmail.com>
2016-05-13 14:27:35 +0530

    CF

---
+++ CodeForces_656_Da_Vinci_Powers.py
@@ -0,0 +1,51 @@
+from sys import stdin as Si
+
+if __name__=='__main__':
+    n =int(Si.readline())
+    if n%2!=0:
+        print(n**2-1)
+    else:
+        print (pow(2,n))
+'''
+
+A. Da Vinci Powers
+time limit per test
+2 seconds
+memory limit per test
+64 megabytes
+input
+standard input
+output
+standard output
+Input
+
+The input contains a single integer a (0 ≤ a ≤ 35).
+Output
+
+Output a single integer.
+Examples
+Input
+
+3
+
+Output
+
+8
+
+Input
+
+10
+
+Output
+
+1024
+
+
+'''
+
+            
+            
+        
+            
+            
+        
