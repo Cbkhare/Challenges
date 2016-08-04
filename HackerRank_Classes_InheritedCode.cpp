32e731449f22059bd028f0aaa8b165b9e96e7511
cbkhare <chitrabasukhare89@gmail.com>
2016-08-04 16:29:47 +0530

    HR_CPP

---
+++ HackerRank_Classes_InheritedCode.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+#include <ostream>
+#include <exception>
+using namespace std;
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+const int INF = 1<<29;
+typedef long long ll;
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+/////////////////////////////////////////////////////////////////////
+
+
+class BadLengthException : public exception{
+
+    private:
+    
+    int n;
+    
+    public:
+    
+    BadLengthException(int N){n = N;}
+    
+    virtual const char * what(){      //virtual const char * what()can also be used, refer to stackovrflow explanation of whyvirtual is not required
+    
+        return std::to_string(n).c_str();
+        
+    }
+    
+};
+
+bool checkUsername(string username) {
+	bool isValid = true;
+	int n = username.length();
+	if(n < 5) {
+		throw BadLengthException(n);
+	}
+	for(int i = 0; i < n-1; i++) {
+		if(username[i] == 'w' && username[i+1] == 'w') {
+			isValid = false;
+		}
+	}
+	return isValid;
+}
+
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		string username;
+		cin >> username;
+		try {
+			bool isValid = checkUsername(username);
+			if(isValid) {
+				cout << "Valid" << '\n';
+			} else {
+				cout << "Invalid" << '\n';
+			}
+		} catch (BadLengthException e) {
+			cout << "Too short: " << e.what() << '\n';
+		}
+	}
+	return 0;
+}
+
+/*
+You inherited a piece of code that performs username validation for your company's website. The existing function works reasonably well, but it throws an exception when the username is too short. Upon review, you realize that nobody ever defined the exception.
+
+The inherited code is provided for you in the locked section of your editor. Complete the code so that, when an exception is thrown, it prints Too short: N (where is the length of the given username).
+
+Input Format
+
+The first line contains an integer, , the number of test cases.
+Each of the subsequent lines describes a test case as a single username string, .
+
+Constraints
+
+    The username consists only of uppercase and lowercase letters.
+
+Output Format
+
+You are not responsible for directly printing anything to stdout. If your code is correct, the locked stub code in your editor will print either Valid 
+(if the username is valid), Invalid (if the username is invalid), or Too short: N (where is the length of the too-short username) 
+on a new line for each test case.
+
+Sample Input
+
+3
+Peter
+Me
+Arxwwz
+
+Sample Output
+
+Valid
+Too short: 2
+Invalid
+
+Explanation
+
+Username Me is too short because it only contains characters, so your exception prints .
+All other validation is handled by the locked code in your editor.
+*/
